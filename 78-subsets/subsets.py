6e1984920c487f7ba837bb9d184cbbc9f4b5bcbf
kanchananarayanswamy <153471390+kanchananarayanswamy@users.noreply.github.com>
2026-06-08 10:39:11 +0530

    Time: 126 ms (1.38%) | Memory: 19.8 MB (5.55%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def fun(i):
+            if i==len(nums):
+                res.append(l.copy())
+                print(res)
+                return
+            #with
+            l.append(nums[i])
+            fun(i+1)
+            #without 
+            l.pop()
+            fun(i+1)     
+        res=[]
+        l=[]
+        fun(0)
+        return res
