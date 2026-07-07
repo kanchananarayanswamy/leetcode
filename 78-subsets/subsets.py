b2b024497188a50d04e761d79bef646e2a4a1c65
kanchananarayanswamy <153471390+kanchananarayanswamy@users.noreply.github.com>
2026-07-07 11:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.65%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,13 +2,10 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res=[]
         def gen_sub(i,sub_set,res):
-            if i==len(nums):
-                res.append(sub_set.copy())
-                return
-            sub_set.append(nums[i])
-            gen_sub(i+1,sub_set,res)
-            sub_set.pop()
-            gen_sub(i+1,sub_set,res)
+            res.append(sub_set.copy())
+            for j in range(i,len(nums)):
+                sub_set.append(nums[j])
+                gen_sub(j+1,sub_set,res)
+                sub_set.pop()
         gen_sub(0,[],res)
         return res
-
