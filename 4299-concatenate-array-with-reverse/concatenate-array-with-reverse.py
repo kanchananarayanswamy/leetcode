629eb0ae87325329cb5e979fc2897252180fdd5d
kanchananarayanswamy <153471390+kanchananarayanswamy@users.noreply.github.com>
2026-07-19 13:09:09 +0530

    Time: 1 ms (25.28%) | Memory: 19.3 MB (14.62%) - LeetSync

---
+++ 4299-concatenate-array-with-reverse/concatenate-array-with-reverse.py
@@ -0,0 +1,8 @@
+class Solution:
+    def concatWithReverse(self, nums: list[int]) -> list[int]:
+        n=len(nums)
+        ans=[0]*(n+n)
+        for i in range(n):
+            ans[i]=nums[i]
+            ans[i+n]=nums[n-i-1]
+        return ans
