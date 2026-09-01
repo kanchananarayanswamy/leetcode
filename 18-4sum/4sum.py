5a9bf255d65b0a8b17300ca36b3cb3fa7b3f9b64
kanchananarayanswamy <153471390+kanchananarayanswamy@users.noreply.github.com>
2026-09-01 12:58:23 +0530

    Time: 396 ms (60.96%) | Memory: 19.3 MB (54.57%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -4,7 +4,7 @@ class Solution:
         nums.sort()
         ans=[]
         for i in range(n):
-            if i>0 and nums[i]==nums [i -1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue 
             for j in range (i+1,n):
                 if j>i+1 and nums[j]==nums[j-1]:
@@ -13,7 +13,7 @@ class Solution:
                 r = n-1 
                 while l <r :
                     s = nums[i]+nums[j]+nums[l]+nums[r]
-                    if s ==target :
+                    if s==target :
                         ans.append((nums[i],nums[j],nums[l],nums[r]))
                         while l<r and nums[l]==nums[l+1]:
                             l+=1
@@ -21,8 +21,8 @@ class Solution:
                             r-=1
                         l+=1
                         r-=1
-                    elif s >target :
-                        r-=1
-                    else :
+                    elif s<target :
                         l+=1
+                    else :
+                        r-=1
         return ans 
