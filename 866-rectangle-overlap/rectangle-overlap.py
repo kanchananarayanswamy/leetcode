bc035d6dda527e2f96b1e9555d8ac00b0dd9f45d
kanchananarayanswamy <153471390+kanchananarayanswamy@users.noreply.github.com>
2026-09-15 09:17:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (58.48%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        x1=rec1[0]
+        y1=rec1[1]
+        x2=rec1[2]
+        y2=rec1[3]
+        x3=rec2[0]
+        y3=rec2[1]
+        x4=rec2[2]
+        y4=rec2[3]
+        if x1>=x4 or y1>=y4 or x3>=x2 or y3>=y2:
+            return False
+        return True
